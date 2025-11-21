b0e31e24bdeea40d5ff730c6c67ead1d2b1450b8
sergio <sfernandezrodriguez@danielcastelao.org>
2025-11-21 10:37:47 +0100

    change

---
+++ tronco conico.py
@@ -0,0 +1,28 @@
+from cono import Cono
+import math
+
+
+
+class TroncoConico (Cono):
+    def __init__(self, x, y, radio,radio2,altura):
+        super().__init__(x,y,radio,altura)
+        self.radio2 = abs(radio2)
+    def obtenerVolumenTR(self,radio2,altura):
+        Vg = super().obtenerVolumen()
+        Vp = 1/3* math.pi*radio2*(super(altura).Cono-self.altura)
+        return Vg - Vp
+    def obtenerSupercifieTR(self,radio2,altura):
+        area = math.pi*self.radio**2+math.pi*self.radio2**2+math.pi*(self.radio+radio2)
+        return area
+    def obtenerGeneratrizTR(self,radio2):
+        g = (super().obtenerGeneratriz() * radio2)/self.radio
+        return super().obtenerGeneratriz() - g
+    def aCadea(self):
+        cadea = super().toString() + "\n\t Radio : " + str(self.radio) + "\n\t Altura : " + str(self.altura)
+        return cadea
+
+
+
+
+cono = TroncoConico(2,3,4,3,7)
+print("cono",cono.aCadea(),"superficie : ",cono.obtenerSupercifie(5),"volumen : ",cono.obtenerVolumen(9),"generatriz : ",cono.obtenerGeneratriz(6))
